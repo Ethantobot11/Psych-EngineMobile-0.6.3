19ab383439283da88e6646ada982d53f171a577f
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-27 15:56:42 +0200

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -71,16 +71,10 @@ import sys.FileSystem;
 import sys.io.File;
 #end
 
-#if VIDEOS_ALLOWED
-#if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
-#elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
-#elseif (hxCodec == "2.6.0") import VideoHandler;
-#else import vlc.MP4Handler as VideoHandler; #end
-#end
-
 import mobile.TouchButton;
 import mobile.TouchPad;
 import mobile.input.MobileInputID;
+import VideoSprite
 
 using StringTools;
 
